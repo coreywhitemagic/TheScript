c502c7b49b1c0eb2f84ea793055c1b4c8d403bbc
coreywhitemagic <141536483+coreywhitemagic@users.noreply.github.com>
2023-08-06 16:18:50 -0400

    Update script.c

--- script.c
+++ script.c
@@ -1,16 +1,12 @@
 #include <iostream>
-
 #include <string>
-
 #include <vector>
-
-#include <algorithm> // for std::min
-
+#include <algorithm>
 #include <fstream>
 
 using namespace std;
 
-double similarityPercentage(const std::string &s1, const std::string &s2) {
+double similarityPercentage(const std::string& s1, const std::string& s2) {
     const size_t len1 = s1.size();
     const size_t len2 = s2.size();
 
@@ -28,7 +24,7 @@ double similarityPercentage(const std::string &s1, const std::string &s2) {
                 dp[i - 1][j] + 1,
                 dp[i][j - 1] + 1,
                 dp[i - 1][j - 1] + (s1[i - 1] == s2[j - 1] ? 0 : 1)
-            });
+                });
         }
     }
 
@@ -51,63 +47,64 @@ int main() {
     }
     answersFile.close();
 
-  std::string planet[] = {
-    "sun",
-    "mercury",
-    "venus",
-    "earth",
-    "mars",
-    "jupiter",
-    "saturn",
-    "uranus",
-    "neptune",
-    "pluto",
-    "full moon",
-    "new moon",
-    "waxing moon",
-    "waning moon",
-    "aries",
-    "taurus",
-    "gemini",
-    "cancer",
-    "leo",
-    "virgo",
-    "libra",
-    "scorpio",
-    "sagittarius",
-    "capricorn",
-    "aquarius",
-    "pisces"
-  };
+    std::string planet[] = {
+      "sun",
+      "mercury",
+      "venus",
+      "earth",
+      "mars",
+      "jupiter",
+      "saturn",
+      "uranus",
+      "neptune",
+      "pluto",
+      "full moon",
+      "new moon",
+      "waxing moon",
+      "waning moon",
+      "aries",
+      "taurus",
+      "gemini",
+      "cancer",
+      "leo",
+      "virgo",
+      "libra",
+      "scorpio",
+      "sagittarius",
+      "capricorn",
+      "aquarius",
+      "pisces"
+    };
 
-  std::string userAnswer;
-  int questionNumber = 0;
-  int attempts = 0;
-  int planetNumber = 0;
+    std::string userAnswer;
+    int questionNumber = 0;
+    int attempts = 0;
+    int planetNumber = 0;
 
-  while (questionNumber < answers.size()) {
-    if(planetNumber > 25){
-      planetNumber = 0;
+    while (questionNumber < answers.size()) {
+        if (planetNumber > 25) {
+            planetNumber = 0;
+        }
+        std::cout << "Line " << questionNumber + 1 << "\n("
+            << planet[planetNumber] << ")\n";
+        std::getline(std::cin, userAnswer);
+        std::cout << std::endl;
+        double similarity = similarityPercentage(userAnswer, answers[questionNumber]);
+        if (similarity > 65) {
+            std::cout << "'"<<answers[questionNumber]<<"'\nCorrect! (" << similarity << "%)\n\n";
+            questionNumber++;
+            planetNumber++;
+        }
+        else {
+            std::cout << "The correct answer was, '"
+                << answers[questionNumber] << "' (" << similarity << "%)\n" <<
+                "Starting over.\n\n";
+            questionNumber = 0;
+            planetNumber = 0;
+        }
     }
-    std::cout << "Line " << questionNumber + 1 << "\n(" 
-    << planet[planetNumber] << ")\n";
-    std::getline(std::cin, userAnswer);
-    std::cout << std::endl;
-    double similarity = similarityPercentage(userAnswer, answers[questionNumber]);
-    if (similarity > 65) {
-      std::cout << "Correct! ("<<similarity<<"%)\n\n";
-      questionNumber++;
-      planetNumber++;
-    } else {
-      std::cout << "The correct answer was, '" 
-      << answers[questionNumber] << "' ("<<similarity<<"%)\n" <<
-        "Starting over.\n\n";
-      questionNumber = 0;
-      planetNumber = 0;
-    }
-  }
 
-  std::cout << "You remembered everything!\n";
+    std::cout << "You remembered everything!\n";
 
-  return 0;
+    return 0;
 }
